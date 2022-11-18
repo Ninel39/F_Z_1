b1b8b6df6122e9df3a210b446feb2668652316d6
Ninel39 <ninel.leynveber@mail.ru>
2022-11-18 22:10:38 +0300

    Добавьте файлы проекта.

---
+++ F_Z_1/F_Z_1.cpp
@@ -0,0 +1,31 @@
+﻿// F_Z_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+/*Написать функцию, которая принимает два параметра: основание степени и показатель
+степени, и вычисляет степень числа на основе полученных данных.
+*/
+
+int powInnumber(int number, int powNum)
+{
+    if (powNum == 0)
+        return 1;
+    if (powNum == 1)
+        return number;
+    int result = number;
+    for (int i = 0; i < powNum - 1; i++)
+        result *= number;
+    return result;
+}
+
+int main()
+{
+    int number, powNum;
+    cout << "Enter number : ";
+    cin >> number;
+    cout << "Enter pow : ";
+    cin >> powNum;
+    cout << "Rezultat = " << powInnumber(number, powNum) << endl;
+}
+
